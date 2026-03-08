6ce018f31a9e19b6b8f21e89458a83ca5462840f
zerotohero510 <praveenkm4473@gmail.com>
2026-03-09 00:41:57 +0530

    Inital Commit From VS Code

---
+++ data/BQ/bronze.sql
@@ -0,0 +1,66 @@
+-- Description: Create external tables for bronze dataset in BigQuery
+-- please do not forget to replace the bucket path
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.departments_ha` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-a/departments/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.encounters_ha` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-a/encounters/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.patients_ha` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-a/patients/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.providers_ha` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-a/providers/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.transactions_ha` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-a/transactions/*.json']
+);
+
+---------------------------------------------------------------------------------------------------------------------------
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.departments_hb` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-b/departments/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.encounters_hb` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-b/encounters/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.patients_hb` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-b/patients/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.providers_hb` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-b/providers/*.json']
+);
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `project-839560af-c436-4801-9dc.bronze_dataset.transactions_hb` 
+OPTIONS (
+  format = 'JSON',
+  uris = ['gs://healthcare-bucket-07032026/landing/hospital-b/transactions/*.json']
+);
+
+---------------------------------------------------------------------------------------------------------------------------
